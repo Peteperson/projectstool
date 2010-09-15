8e226ce914446eeac2ef795af380b91a532c728a
peteperson2000 <peteperson2000@5f145e9e-ed63-0a0f-4c59-577c0f42e01f>
2010-09-15 13:45:25 +0000

    Check before import.

--- WebPart/Projects/MassiveImport.aspx.vb
+++ WebPart/Projects/MassiveImport.aspx.vb
@@ -16,6 +16,12 @@ Partial Class MassiveImport
     Private MIdescription, MIcomments As String
 
     Protected Sub btnImport_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnImport.Click
+        If ImportData(True) Then ImportData(False) Else lblResults.InnerHtml += "Process aborted. All records <U>were not imported<U/>.<br/>"
+    End Sub
+
+    Private Function ImportData(ByVal PerformCheck As Boolean) As Boolean
+        lblResults.InnerHtml = IIf(PerformCheck, "Check results:<br />", "Import results:<br />")
+        Dim ret As Boolean = True
         Dim line() As String = txtData.Text.Split(vbCrLf.ToCharArray, StringSplitOptions.RemoveEmptyEntries)
         Dim items() As String
         For i As Integer = 0 To line.Length - 1
@@ -27,12 +33,13 @@ Partial Class MassiveImport
             MIresponsible1 = Nothing
             MIresponsible2 = Nothing
             items = line(i).Split(vbTab)
-            If items.Length = 7 Then
+            If items.Length = 7 Or items.Length = 6 Then
                 Try
                     If Not ddlProjects.Items.FindByValue(items(0)) Is Nothing Then
                         MIprojectId = items(0)
                     Else
                         lblResults.InnerHtml += "Line " & i + 1 & " <b>error:</b>Wrong ProjectId.<br/>"
+                        ret = False
                         Exit Try
                     End If
                     MIdescription = items(1)
@@ -40,12 +47,14 @@ Partial Class MassiveImport
                         MIresponsible1 = items(2)
                     Else
                         lblResults.InnerHtml += "Line " & i + 1 & " <b>error:</b>Wrong User1Id.<br/>"
+                        ret = False
                         Exit Try
                     End If
                     If Not ddlUsers.Items.FindByValue(items(3)) Is Nothing Then
                         MIresponsible2 = items(3)
                     Else
                         lblResults.InnerHtml += "Line " & i + 1 & " <b>error:</b>Wrong User2Id.<br/>"
+                        ret = False
                         Exit Try
                     End If
                     MIdeadline = items(4)
@@ -53,19 +62,23 @@ Partial Class MassiveImport
                         MIstatus = items(5)
                     Else
                         lblResults.InnerHtml += "Line " & i + 1 & " <b>error:</b>Wrong StatusId.<br/>"
+                        ret = False
                         Exit Try
                     End If
-                    MIcomments = items(6)
-                    sqldsAP.Insert()
+                    If items.Length = 7 Then MIcomments = items(6) Else MIcomments = ""
+                    If Not PerformCheck Then sqldsAP.Insert()
                     lblResults.InnerHtml += "Line " & i + 1 & " <b>Ok</b>.<br/>"
                 Catch ex As Exception
                     lblResults.InnerHtml += "Line " & i + 1 & " <b>error:</b>" & ex.Message & "<br/>"
+                    ret = False
                 End Try
             Else
                 lblResults.InnerHtml += "Line " & i + 1 & " <b>error:</b> Wrong line format.<br/>"
+                ret = False
             End If
         Next
-    End Sub
+        Return ret
+    End Function
 
     Protected Sub sqldsAP_Inserting(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.SqlDataSourceCommandEventArgs) Handles sqldsAP.Inserting
         e.Command.Parameters("@ProjectId").Value = MIprojectId
