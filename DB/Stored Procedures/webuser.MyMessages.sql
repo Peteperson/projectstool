c76cc4621edf8e5acd9063b1f414490cc3107caf
peteperson2000 <peteperson2000@5f145e9e-ed63-0a0f-4c59-577c0f42e01f>
2010-10-12 14:29:53 +0000

    Replace drop down lists with text (joins)

--- DB/Stored Procedures/webuser.MyMessages.sql
+++ DB/Stored Procedures/webuser.MyMessages.sql
@@ -6,8 +6,13 @@ GO
 CREATE PROCEDURE [webuser].[MyMessages]
     @UserId INT 
 AS 
-    SELECT [Messages].*, Users.LastName + ' ' + users.FirstName AS Fullname FROM [Messages] 
-    INNER JOIN Users ON [Messages].Writer = Users.id
+    SELECT [Messages].*, Users.LastName + ' ' + users.FirstName AS Fullname 
+		, ISNULL(t2.LastName + ' ' + t2.FirstName, '') AS ToUserText
+		, ISNULL(companies.Name, '') AS ToCompanyText
+    FROM webuser.[Messages] 
+    INNER JOIN webuser.Users ON [Messages].Writer = Users.id
+    LEFT JOIN webuser.Users t2 ON [Messages].ToUserId = t2.id
+    LEFT JOIN webuser.companies ON webuser.Companies.Id = webuser.Messages.ToCompanyId
     WHERE Writer = @UserId
     ORDER BY id DESC 
 GO
