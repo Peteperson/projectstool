167b1746259782978a5bfbe7af469e0e9dcd3346
peteperson2000 <peteperson2000@5f145e9e-ed63-0a0f-4c59-577c0f42e01f>
2010-10-18 15:24:50 +0000

    One user many companies

---
+++ DB/Tables/webuser.UsersCompanies.sql
@@ -0,0 +1,18 @@
+CREATE TABLE [webuser].[UsersCompanies]
+(
+[UserId] [int] NOT NULL,
+[CompanyId] [int] NOT NULL,
+[Position] [int] NOT NULL,
+[Telephone] [nvarchar] (13) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_UsersCompanies_Telephone] DEFAULT (''),
+[Mobile] [nvarchar] (13) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_UsersCompanies_Mobile] DEFAULT (''),
+[Email] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_UsersCompanies_Email] DEFAULT ('')
+) ON [PRIMARY]
+GO
+ALTER TABLE [webuser].[UsersCompanies] ADD CONSTRAINT [PK_UsersCompanies] PRIMARY KEY CLUSTERED  ([UserId], [CompanyId]) ON [PRIMARY]
+GO
+ALTER TABLE [webuser].[UsersCompanies] ADD CONSTRAINT [FK_UsersCompanies_Companies] FOREIGN KEY ([CompanyId]) REFERENCES [webuser].[Companies] ([Id])
+GO
+ALTER TABLE [webuser].[UsersCompanies] ADD CONSTRAINT [FK_UsersCompanies_VariousTypes] FOREIGN KEY ([Position]) REFERENCES [webuser].[VariousTypes] ([id])
+GO
+ALTER TABLE [webuser].[UsersCompanies] ADD CONSTRAINT [FK_UsersCompanies_Users] FOREIGN KEY ([UserId]) REFERENCES [webuser].[Users] ([id])
+GO
