a7813175d7d1a5c4f8a31633a0ea9cbcc1169cfe
peteperson2000 <peteperson2000@5f145e9e-ed63-0a0f-4c59-577c0f42e01f>
2010-11-25 15:13:33 +0000

    Timeout issue and new printing approach

--- WebPart/Projects/ViewErrors.aspx.vb
+++ WebPart/Projects/ViewErrors.aspx.vb
@@ -8,4 +8,18 @@ Partial Class ViewErrors
             clndFrom.SelectedDate = Today
         End If
     End Sub
+
+    Protected Sub Page_LoadComplete(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.LoadComplete
+        Dim out As String = ""
+        Dim t As Type = Session.GetType
+        Dim pi() As System.Reflection.PropertyInfo = t.GetProperties
+        For Each propinfo In pi
+            Try
+                out &= propinfo.Name.ToString & " = " & propinfo.GetValue(Session, Nothing).ToString & "<br/>"
+            Catch ex As Exception
+                out &= "error<br/>"
+            End Try
+        Next
+        litInfo.Text = out
+    End Sub
 End Class
